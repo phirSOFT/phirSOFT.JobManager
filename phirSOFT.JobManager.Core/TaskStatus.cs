85ce07e1c36b89dc2ab15e9f91e3a168073bbd15
Philemon Eichin <phileicht@outlook.de>
2018-02-07 21:27:00 +0100

    Add TaskStatus

---
+++ phirSOFT.JobManager.Core/TaskStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace phirSOFT.JobManager.Core
+{
+    /// <summary>
+    /// States of a job
+    /// </summary>
+    public enum JobStatus
+    {
+        /// <summary>
+        /// The job is currently executed.
+        /// </summary>
+        Running,
+
+        /// <summary>
+        /// The execution of the job is paused.
+        /// </summary>
+        Paused,
+
+        /// <summary>
+        /// The job sucessfully completed.
+        /// </summary>
+        Succeded,
+
+        /// <summary>
+        /// An Error occured during the execution of the job.
+        /// </summary>
+        Faulted
+    }
+}
