a418fa427eea7d6daf791868bb847780e8d14ed7
Philemon Eichin <phileicht@gmx.de>
2018-02-13 20:49:36 +0100

    Fix recursive lock
    Fix Situaion, when a lock was aquired twice.

--- phirSOFT.JobManager.Core/JobManager.cs
+++ phirSOFT.JobManager.Core/JobManager.cs
@@ -37,8 +37,9 @@ namespace phirSOFT.JobManager.Core
         {
             get
             {
+                var status = OverallStatus;
                 _registredJobsLock.EnterReadLock();
-                var result = _registredJobs.Where(job => job.Status == OverallStatus && job.SupportProgress)
+                var result = _registredJobs.Where(job => job.Status == status && job.SupportProgress)
                     .Average(job => job.Progress);
                 _registredJobsLock.ExitReadLock();
                 return result;
@@ -49,8 +50,9 @@ namespace phirSOFT.JobManager.Core
         {
             get
             {
+                var status = OverallStatus;
                 _registredJobsLock.EnterReadLock();
-                var result = _registredJobs.Any(job => job.Status == OverallStatus && job.SupportProgress);
+                var result = _registredJobs.Any(job => job.Status == status && job.SupportProgress);
                 _registredJobsLock.ExitReadLock();
                 return result;
             }
